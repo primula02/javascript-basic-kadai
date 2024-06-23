01fa0e09c402e8c4957586dcfd77abf0034fc23c
primula02 <167946893+primula02@users.noreply.github.com>
2024-06-23 13:09:47 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+const addBtn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+setTimeout(() => {
+  text.textContent = 'ボタンをクリックしました';
+}, 2000);
+});
+
+
